3fb64e340eb7637b0d7a06fb905d6b3b2586ffbf
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-08 09:34:21 +0100

    menù

--- Program.cs
+++ Program.cs
@@ -4,9 +4,21 @@
     {
         static void Main(string[] args)
         {
+            int scelta;
             
+            do
+            {
+                Console.WriteLine("1) Inserimento completo nominativo");
+                Console.WriteLine("2) Elenco dei nominativi inseriti");
+                Console.WriteLine("3) Ricerca del nominativo in base al nickname");
+                Console.WriteLine("9) Esci");
+                Console.WriteLine("\nInserisci la tua scelta:");
+                scelta = int.Parse(Console.ReadLine());
+            } while (scelta != 9);
 
 
         }
+
+        
     }
 }
