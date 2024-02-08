c770f155c818847afd859612f701591ef04c6897
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-08 21:22:13 +0100

    controlli valori inseriti dall'utente

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@
 
             for (int i = 0; i<100 ; i++)
             {
-                Console.WriteLine("\n1) Inserimento completo nominativo");
+                Console.WriteLine("1) Inserimento completo nominativo");
                 Console.WriteLine("2) Elenco dei nominativi inseriti");
                 Console.WriteLine("3) Ricerca del nominativo in base al nickname");
                 Console.WriteLine("9) Esci");
@@ -27,26 +27,48 @@
                 switch (scelta)
                 {
                     case 1:
-                        Console.WriteLine("\ninserimento nominativo completo:");
+                        Console.WriteLine("\ninserimento nominativo completo:\n");
                         Console.WriteLine("inserisci il tuo nome (obbligatorio):");
                         nome[i] = Console.ReadLine();
+                        while (string.IsNullOrEmpty(nome[i]))
+                        {
+                            Console.WriteLine("nome non valido inserirlo nuovamente:");
+                            nome[i] = Console.ReadLine();
+                        }
                         Console.WriteLine("inserisci il tuo cognome (obbligatorio):");
                         cognome[i] = Console.ReadLine();
+                        while (string.IsNullOrEmpty(cognome[i]))
+                        {
+                            Console.WriteLine("cognome non valido inserirlo nuovamente:");
+                            cognome[i] = Console.ReadLine();
+                        }
                         Console.WriteLine("inserisci la email (facoltativo):");
                         email[i] = Console.ReadLine();
                         Console.WriteLine("inserisci il tuo nickname (obbligatorio)");
                         nickname[i] = Console.ReadLine();
+                        while (string.IsNullOrEmpty(nickname[i]))
+                        {
+                            Console.WriteLine("nickname non valido inserirlo nuovamente:");
+                            nickname[i] = Console.ReadLine();
+                        }
                         Console.WriteLine("inserisci la tua età (facoltativo):");
                         eta[i] = int.Parse(Console.ReadLine());
-                        Console.WriteLine("inserisci la tua simpatia (facoltativo):");
+      
+                        Console.WriteLine("inserisci la tua simpatia (facoltativo compreso tra 1 e 5):");
                         simpatia[i] = int.Parse(Console.ReadLine());
+                        while (simpatia[i]<1 || simpatia[i] > 5)
+                        {
+                            Console.WriteLine("valore per la simpatia inserito non valido, inserirlo nuovamente");
+                            simpatia[i] = int.Parse(Console.ReadLine());
+                        }
                         break;
 
                     case 2:
                         Console.WriteLine("\nelenco di tutti i nominativi inseriti:\n");
-                        
+ 
                         for(int j=0; j<i ; j++)
                         {
+                            Console.WriteLine($"nominativo {j + 1}:");
                             Console.WriteLine($"nome: {nome[j]}");
                             Console.WriteLine($"cognome: {cognome[j]}");
                             Console.WriteLine($"email: {email[j]}");
