dad01d33f7e3dbcf0d8cebfc6949f1779c30445a
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-02-20 17:38:57 +0100

    ricerca per nome case insensitive

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace AS2324.SpadiniLorenzo.Rubrica
+﻿using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AS2324.SpadiniLorenzo.Rubrica
 {
     internal class Program
     {
@@ -12,17 +15,20 @@
             int[] eta = new int[100];
             int[] simpatia = new int[100];
 
-            for (int i = 0; i<100 ; i++)
+            for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine("1) Inserimento completo nominativo");
                 Console.WriteLine("2) Elenco dei nominativi inseriti");
                 Console.WriteLine("3) Ricerca del nominativo in base al nickname");
+                Console.WriteLine("4) Ricerca di tutti i nomi in base al nome con ricerca case insentive");
+                Console.WriteLine("5) Ricerca di tutti i cognomi che iniziano per a prescindere da come è scritto ");
+                Console.WriteLine("6) Ricerca di tutti i cognomi che contengono il dato ricercato a prescindere da come è scritto");
                 Console.WriteLine("9) Esci");
                 Console.WriteLine("\nInserisci la tua scelta:");
                 scelta = int.Parse(Console.ReadLine());
 
                 if (scelta == 9)
-                   break;
+                    break;
 
                 switch (scelta)
                 {
@@ -53,10 +59,10 @@
                         }
                         Console.WriteLine("inserisci la tua età (facoltativo):");
                         eta[i] = int.Parse(Console.ReadLine());
-      
+
                         Console.WriteLine("inserisci la tua simpatia (facoltativo compreso tra 1 e 5):");
                         simpatia[i] = int.Parse(Console.ReadLine());
-                        while (simpatia[i]<1 || simpatia[i] > 5)
+                        while (simpatia[i] < 1 || simpatia[i] > 5)
                         {
                             Console.WriteLine("valore per la simpatia inserito non valido, inserirlo nuovamente");
                             simpatia[i] = int.Parse(Console.ReadLine());
@@ -65,8 +71,8 @@
 
                     case 2:
                         Console.WriteLine("\nelenco di tutti i nominativi inseriti:\n");
- 
-                        for(int j=0; j<i ; j++)
+
+                        for (int j = 0; j < i; j++)
                         {
                             Console.WriteLine($"nominativo {j + 1}:");
                             Console.WriteLine($"nome: {nome[j]}");
@@ -75,14 +81,14 @@
                             Console.WriteLine($"nickname: {nickname[j]}");
                             Console.WriteLine($"eta: {eta[j]}");
                             Console.WriteLine($"simpatia: {simpatia[j]}\n");
-                        } 
+                        }
                         break;
 
-                     case 3:
+                    case 3:
                         Console.WriteLine("\nricerca nominativo in base la nickname:");
                         Console.WriteLine("\ninserisci il nickname:");
                         string ricercaNickname = Console.ReadLine();
-                        for(int t=0; t<i; t++ ) 
+                        for (int t = 0; t < i; t++)
                         {
                             if (ricercaNickname == nickname[t])
                             {
@@ -93,9 +99,24 @@
                                 Console.WriteLine($"eta: {eta[t]}");
                                 Console.WriteLine($"simpatia: {simpatia[t]}\n");
                             }
-
                         }
                         break;
+
+                    case 4:
+                        Console.WriteLine("Inserisci il nome che vuoi ricercare (case insensitive):");
+                        string nomeRicerca = Console.ReadLine();
+                        bool trovato = false;
+                        foreach (string nomeCorrente in nome)
+                        {
+                            if (nomeCorrente != null && nomeRicerca.ToUpper() == nomeCorrente.ToUpper())
+                            {
+                                Console.WriteLine($"Nome: {nomeCorrente}");
+                                trovato = true;
+                            }
+                        }
+                        if (!trovato) Console.WriteLine("Nome non trovato.");
+                        break;
+
                 }
             }
         }
